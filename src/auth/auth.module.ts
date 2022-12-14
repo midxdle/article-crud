3b3e3c8712596b32c51c47fe133c20efb35739da
midxdle <sina.heydarian.sh@gmail.com>
2022-12-15 02:05:54 +0330

    just save everything

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { UserService } from 'src/user/user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secretOrPrivateKey: 'secret123456789',
+    }),
+  ],
+  providers: [UserService, AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
